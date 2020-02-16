3514479966060469d55f62e1a7f161b63419d20a
mraffiramdhani <raffi.programming@gmail.com>
2020-02-16 12:16:35 +0700

    setup redux state management

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "./redux/store";
+
+const { store, persistor } = storage();
+
+const Main = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  );
+};
+
+ReactDOM.render(<Main />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/redux/reducer/auth.js
@@ -0,0 +1,15 @@
+const initialState = {
+  data: [],
+  isLoading: false,
+  isError: false,
+  isSuccess: false
+};
+
+const auth = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default auth;
